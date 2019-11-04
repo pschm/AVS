773d4a53f029a8c202ecc7d47ef2196aad5604d7
RedAvenge <thepowerredstone@gmail.com>
2019-11-04 18:18:08 +0100

    Add store walls and shelf texts

---
+++ unity/AVS-Supermarkt_Frontend/Assets/Scripts/Shelf.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[ExecuteInEditMode]
+public class Shelf : MonoBehaviour {
+
+    public string productName;
+
+    private TextMeshPro textPlane;
+
+
+    private void Update() {
+#if UNITY_EDITOR
+        if(transform.hasChanged && textPlane != null) {
+            var rect = textPlane.GetComponent<RectTransform>();
+            if(transform.localScale.x != 0) rect.localScale = new Vector3(1 / transform.localScale.x, rect.localScale.y, rect.localScale.z);
+        }
+#endif
+
+
+
+    }
+
+
+    private void OnValidate() {
+        if(textPlane == null) textPlane = transform.GetComponentInChildren<TextMeshPro>();
+        textPlane.text = productName;       
+    }
+}

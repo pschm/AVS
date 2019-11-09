ca6a20a9c8bb8b6bd57e79dcd73e9114116424cf
pschm <philipp.schmeier@gmail.com>
2019-11-09 16:44:19 +0100

    The scheduler now provides the full api according to the readme

--- scheduler/src/main/kotlin/Scheduler.kt
+++ scheduler/src/main/kotlin/Scheduler.kt
@@ -1,18 +1,15 @@
+import com.google.gson.Gson
 import genetic_algorithm.Individual
+import io.ktor.application.ApplicationCall
 import io.ktor.application.call
-import io.ktor.client.HttpClient
-import io.ktor.client.request.get
-import io.ktor.client.request.post
-import io.ktor.client.request.url
 import io.ktor.features.origin
-import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
-import io.ktor.http.contentType
 import io.ktor.request.ContentTransformationException
 import io.ktor.request.receive
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.post
+import io.ktor.routing.put
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
@@ -23,97 +20,141 @@ import io.ktor.server.netty.NettyApplicationEngine
  */
 class Scheduler(private val unityUrl: String) {
 
-    private val server : NettyApplicationEngine
+    private val server: NettyApplicationEngine
     private val workers = mutableListOf<Worker>()
     private var bestIndividual: Individual? = null
+    private var map: String? = null
+        get() = if (field.isNullOrBlank()) null else field
+
 
     init {
         server = embeddedServer(Netty, port = 8080) {
             routing {
-                get("/") {
+                // enable REST-Calls
+                get("/ping") {
                     val host = call.request.origin.host
-                    println("Request from $host")
-                    call.respondText("Hello World")
+                    println("Ping-Request from $host")
+                    call.respondText("pong")
                 }
 
-                post("/worker") {
-                    // parse Individual from json
-                    val individual = try {
-                        call.receive<Individual>()
-                    } catch (e: ContentTransformationException) {
-                        call.respondText("couldn't update worker, check json", status = HttpStatusCode.BadRequest)
-                        return@post
-                    }
+                // worker
+                post("/worker") { addWorker(call) }
+                put("/worker") { updateWorker(call) }
 
-                    val workerAddress = call.request.origin.host
-                    val worker = updateOrAddWorker(workerAddress, individual)
+                // map
+                get("/map") { respondMap(call) }
+                post("/map") { saveMap(call) }
 
-                    // TODO check, if fitness is always greater than 0
-                    if (worker.individual.fitness > bestIndividual?.fitness ?: -1) {
-                        bestIndividual = worker.individual
-                        sendIndividualToUnity(bestIndividual)
-                    }
-
-                    call.respondText("updated worker individual", status = HttpStatusCode.OK)
-                }
-
-                get("/map") {
-                    call.respondText(getMapJsonFromUnity())
-                }
+                // path
+                get("/path") { respondPath(call) }
             }
         }
-    }
-
-    /**
-     * send new individual to the server and print server feedback
-     *
-     * TODO should the scheduler transform the Individual to a route?
-     */
-    private suspend fun sendIndividualToUnity(individual: Individual?) {
-        val bodyContent = individual ?: return
-
-        val client = HttpClient()
-        val urlString = "$unityUrl/route" // TODO use correct jsonPath
-
-        val message = client.post<Individual> {
-            url(urlString)
-            contentType(ContentType.Application.Json)
-            body = bodyContent
-        }
-
-        // comment for debug
-        println("CLIENT: Message from the server: $message")
-
-        client.close()
     }
 
     fun start() = server.start(wait = true)
 
-    private suspend fun getMapJsonFromUnity(): String {
-        val client = HttpClient()
-
-        // TODO use correct jsonPath
-        return client.get("$unityUrl/map")
+    /**
+     * responds path (200) or error (204) if the path is not yet available
+     */
+    private suspend fun respondPath(call: ApplicationCall) {
+        bestIndividual?.let {
+            val json = Gson().toJson(it)
+            call.respondText(json, status = HttpStatusCode.OK)
+        } ?: call.respondText("The Path is not set yet, try later", status = HttpStatusCode.NoContent)
     }
 
     /**
-     * update [individual] of the worker with given [ipAddress] or add a new worker to [workers]
-     *
-     * @return updated or newly created [Worker]
+     * save sent json to map
      */
-    private fun updateOrAddWorker(ipAddress: String, individual: Individual): Worker {
-        workers.any {
-            if (it.ipAddress == ipAddress) {
-                it.individual = individual
-                return it
-            }
-            else false
+    private suspend fun saveMap(call: ApplicationCall) {
+        println("POST map")
+        map = try {
+            call.receive()
+        } catch (e: Exception) {
+            println("Could not read map")
+            e.printStackTrace()
+            null
         }
 
-        val worker = Worker(ipAddress, individual)
-        workers.add(worker)
-
-        return worker
+        map?.let {
+            call.respondText(it, status = HttpStatusCode.OK)
+        } ?: call.respondText("Could not read json", status = HttpStatusCode.BadRequest)
     }
 
+    /**
+     * responds map data (200) or error (204) if map is not yet available
+     */
+    private suspend fun respondMap(call: ApplicationCall) {
+        map?.let {
+            call.respondText(it, status = HttpStatusCode.OK)
+        } ?: call.respondText("Map is not set yet, try later", status = HttpStatusCode.NoContent)
+    }
+
+    /**
+     * update [Individual] of the calling worker
+     */
+    private suspend fun updateWorker(call: ApplicationCall) {
+        // check if worker is in list
+        val individual = parseIndividual(call) ?: return
+        val workerAddress = call.request.origin.host
+
+        // update worker individual
+        var alreadyInList = false
+        workers.forEach {
+            if (it.ipAddress == workerAddress) {
+                it.individual = individual
+                updateBestIndividual(individual)
+                alreadyInList = true
+                return@forEach
+            }
+        }
+
+        if (alreadyInList)
+            call.respondText("Worker is not registered. Use POST instead", status = HttpStatusCode.BadRequest)
+        else
+            call.respondText("updated individual successfully", status = HttpStatusCode.OK)
+    }
+
+    /**
+     * parse json from [call] and try to add a worker to the list
+     */
+    private suspend fun addWorker(call: ApplicationCall) {
+        val individual = parseIndividual(call) ?: return
+        val workerAddress = call.request.origin.host
+        val worker = Worker(workerAddress, individual)
+
+        // check if the worker is already registered
+        if (workers.any { it.ipAddress == worker.ipAddress }) {
+            call.respondText("Worker is already registered. Use PUT instead", status = HttpStatusCode.BadRequest)
+            return
+        }
+
+        // save worker
+        workers.add(worker)
+
+        // update best individual
+        updateBestIndividual(worker.individual)
+
+        call.respondText("added worker successfully", status = HttpStatusCode.OK)
+    }
+
+    /**
+     * Try to parse an [Individual] from send json in [call].
+     * If no [Individual] could be parsed the call responds with [HttpStatusCode.BadRequest]
+     * @return send [Individual] or null
+     */
+    private suspend fun parseIndividual(call: ApplicationCall): Individual? = try {
+        call.receive<Individual>()
+    } catch (e: ContentTransformationException) {
+        call.respondText("couldn't read json", status = HttpStatusCode.BadRequest)
+        null
+    }
+
+    /**
+     * update [bestIndividual] if [individual] has better fitness
+     */
+    private fun updateBestIndividual(individual: Individual) {
+        if (individual.fitness > bestIndividual?.fitness ?: -1)
+            bestIndividual = individual
+    }
 }

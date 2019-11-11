cbac7b3ea718cea348312f57d636178826e59388
RedAvenge <thepowerredstone@gmail.com>
2019-11-11 20:42:49 +0100

    Add implementation for rest calls

---
+++ unity/AVS-Supermarkt_Frontend/Assets/Scripts/Rest/NodeModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class NodeModel {
+
+    public string name;
+    public Vector2 position;
+    
+    public static List<NodeModel> CreateList(List<Shelf> shelves) {
+        var nodes = new List<NodeModel>();
+        shelves.ForEach(x => nodes.Add(new NodeModel() {
+            name = x.productName,
+            position = new Vector2(x.GetWalkToPoint().x, x.GetWalkToPoint().z)
+        }));
+
+        return nodes;
+    }
+
+}
---
+++ unity/AVS-Supermarkt_Frontend/Assets/Scripts/Rest/SchedulerRestClient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public static class SchedulerRestClient {
+    
+    public static readonly string url = "http://localhost:8080";
+
+
+    public static long PostShoppingList(List<NodeModel> nodes) {
+        var jsonBody = JsonHelper.ToJson(nodes);
+        UnityWebRequest www = UnityWebRequest.Post(url + "/map", jsonBody);
+        www.SetRequestHeader("Accept", "application/json");
+
+        www.SendWebRequest();
+        if(www.isNetworkError) return -1;
+
+        return www.responseCode;
+    }
+
+
+    public static List<NodeModel> GetCalculatedWaypoints() {
+        UnityWebRequest www = UnityWebRequest.Get(url + "/waypoints");
+        www.SetRequestHeader("Accept", "application/json");
+
+        www.SendWebRequest();
+
+        if(www.isNetworkError || www.responseCode != 200) {
+            return null;
+        } else {
+            var data = www.downloadHandler.text;
+            var convertedData = JsonUtility.FromJson<List<NodeModel>>(data);
+
+            return convertedData;
+        }
+    }
+
+
+}

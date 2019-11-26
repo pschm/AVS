ebb0b88425cbf8eb8c60b71e123712cbbef712db
RedAvenge <thepowerredstone@gmail.com>
2019-11-26 15:18:29 +0100

    Add overview visualization for calculated path

--- unity/AVS-Supermarkt_Frontend/Assets/Scripts/Customer.cs
+++ unity/AVS-Supermarkt_Frontend/Assets/Scripts/Customer.cs
@@ -12,11 +12,13 @@ public class Customer : MonoBehaviour {
     private float grabCooldown;
 
     private NavMeshAgent agent;
+    private LineRenderer lineRenderer;
     private Vector3 initialPos;
     private Action finishAction;
 
     private void Awake() {
         agent = GetComponent<NavMeshAgent>();
+        lineRenderer = GetComponent<LineRenderer>();
         agent.isStopped = true;
         initialPos = transform.position;
     }
@@ -64,11 +66,42 @@ public class Customer : MonoBehaviour {
         agent.isStopped = false;
 
         this.finishAction = finishAction;
+
+        SetupLineRenderer(waypoints);
+    }
+
+    public void SetupLineRenderer(List<Vector3> waypoints) {
+        List<Vector3> linePoints = new List<Vector3>();
+        NavMeshPath path = new NavMeshPath();
+
+        //Calculate a path from the start point to the first shelf
+        if(NavMesh.CalculatePath(transform.position, waypoints[0], NavMesh.AllAreas, path)) {
+            foreach(var point in path.corners){
+                linePoints.Add(new Vector3(point.x, point.y + 1, point.z));
+            }
+        }
+
+        //Calculate a path from each self to the next one
+        for(int i = 0; i < waypoints.Count - 1; i++) {
+            if(NavMesh.CalculatePath(waypoints[i], waypoints[i+1], NavMesh.AllAreas, path)) {
+
+                foreach(var point in path.corners) {
+                    linePoints.Add(new Vector3(point.x, point.y + 1, point.z));
+                }
+
+            }
+
+        }
+
+        lineRenderer.positionCount = linePoints.Count;
+        lineRenderer.SetPositions(linePoints.ToArray());
     }
 
     public void ResetPosition() {
         agent.isStopped = true;
         transform.position = initialPos;
+
+        lineRenderer.positionCount = 0;
     }
 
 }

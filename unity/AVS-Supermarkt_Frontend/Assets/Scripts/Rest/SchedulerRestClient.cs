67119dc867780258fba30b07fa5022d5603916ac
RedAvenge <thepowerredstone@gmail.com>
2019-12-03 17:35:30 +0100

    Refactor code and add ip address field to ui

---
+++ unity/AVS-Supermarkt_Frontend/Assets/Scripts/Rest/SchedulerRestClient.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SchedulerRestClient : MonoBehaviour {
+
+    public float delayBetweenRequests = 2f;
+
+    private bool calculationActive;
+
+    public static SchedulerRestClient Instance { get; private set; }
+
+    void Awake() {
+        if(Instance == null) {
+            Instance = this;
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+
+    public void StartCalculationForShoppinglist(List<NodeModel> nodes, string hostUrl, Action<List<NodeModel>> actionOnResult) {
+        if(calculationActive) {
+            throw new NotImplementedException("Caculation still active. Cannot start a new one!");
+        }
+
+        calculationActive = true;
+        if(string.IsNullOrWhiteSpace(hostUrl)) StartCoroutine(DoCalculationEmulated(nodes, actionOnResult));
+        else StartCoroutine(DoCalculation(nodes, hostUrl, actionOnResult));
+    }
+
+
+    private IEnumerator DoCalculationEmulated(List<NodeModel> nodes, Action<List<NodeModel>> actionOnResult) {
+        Debug.Log("Emulating Scheduler is enabled.");
+        yield return new WaitForSeconds(UnityEngine.Random.Range(2, 6));
+
+        Debug.Log("Got result.");
+        calculationActive = false;
+        actionOnResult(nodes);
+    }
+
+
+    private IEnumerator DoCalculation(List<NodeModel> nodes, string hostUrl, Action<List<NodeModel>> actionOnResult) {
+        var request = CreatePostShoppingListRequest(nodes, hostUrl);
+        yield return request.SendWebRequest();
+
+        if(request.responseCode != 200) {
+            Debug.LogWarning("Error: " + request.responseCode);
+            throw new System.NotImplementedException("No error handling for given response code while POST shopping list");
+
+        } else {
+            Debug.Log("Sending to scheduler successful.");
+            yield return QueryCalculationResult(hostUrl, actionOnResult);
+        }
+    }
+
+    private IEnumerator QueryCalculationResult(string hostUrl, Action<List<NodeModel>> actionOnResult) {
+        Debug.Log("Checking for result...");
+        bool isCalculating = true;
+        List<NodeModel> result = null;
+
+        while(result == null && isCalculating) {
+            yield return new WaitForSeconds(delayBetweenRequests);
+
+            var request = CreateGetCalculatedWaypointsRequest(hostUrl);
+            yield return request.SendWebRequest();
+
+            if(!request.isNetworkError && request.responseCode == 200) {
+                Debug.Log("Got result.");
+
+                var response = Encoding.UTF8.GetString(request.downloadHandler.data);
+                result = JsonHelper.FromJson<NodeModel>(response);
+
+            } else if(request.isNetworkError /*|| request.responseCode == 503*/) {
+                Debug.LogWarning($"Cant get result. Network-Error: {request.isNetworkError}, Response-Code: {request.responseCode}");
+                isCalculating = false;
+            }
+        }
+
+        calculationActive = false;
+        actionOnResult(result);
+    }
+
+
+    private static UnityWebRequest CreatePostShoppingListRequest(List<NodeModel> nodes, string hostUrl) {
+        var jsonBody = JsonHelper.ToJson(nodes);
+
+        var request = new UnityWebRequest(hostUrl + "/map", "POST");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonBody);
+
+        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json;charset=utf-8");
+
+        return request;
+    }
+
+    private static UnityWebRequest CreateGetCalculatedWaypointsRequest(string hostUrl) {
+        UnityWebRequest request = UnityWebRequest.Get(hostUrl + "/path");
+        request.SetRequestHeader("Accept", "application/json");
+
+        return request;
+    }
+
+}
--- unity/AVS-Supermarkt_Frontend/Assets/Scripts/UI/UiManager.cs
+++ unity/AVS-Supermarkt_Frontend/Assets/Scripts/UI/UiManager.cs
@@ -1,10 +1,7 @@
-﻿#define EMULATE_SCHEDULER
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using System.Text;
 
 public class UiManager : MonoBehaviour {
 
@@ -12,6 +9,7 @@ public class UiManager : MonoBehaviour {
     public GameObject loadingPanel;
     public GameObject openPlannerPanel;
     public ResultUI resultPanel;
+    public TMP_InputField schedulerIpField;
 
     public Customer customer;
 
@@ -26,8 +24,24 @@ public class UiManager : MonoBehaviour {
 
         var nodes = NodeModel.CreateList(plannerPanel.GetShoppingList());
         Debug.Log("Posting shoppping list...");
-        StartCoroutine(HandleCalculation(nodes));
+
+        var hostUrl = schedulerIpField.text;
+        if(!string.IsNullOrWhiteSpace(hostUrl) && !hostUrl.StartsWith("http")) hostUrl = "http://" + hostUrl;
+        SchedulerRestClient.Instance.StartCalculationForShoppinglist(nodes, hostUrl, ProcessCalculationResult);
     }
+
+    private void ProcessCalculationResult(List<NodeModel> result) {
+        if(result == null || result.Count <= 0) {
+            Debug.Log("Got no result to display.");
+            OpenOpenerUi();
+            return;
+        }
+
+        CloseAllUis();
+        resultPanel.SetResult(result);
+        resultPanel.gameObject.SetActive(true);
+    }
+
 
     public void StartVisualization() {
         CloseAllUis();
@@ -42,71 +56,6 @@ public class UiManager : MonoBehaviour {
         var points = new List<Vector3>();
         resultPanel.ResultNodeList.ForEach(x => points.Add(new Vector3(x.position.x, 0, x.position.y)));
         customer.SetWaypoints(points, OpenOpenerUi);
-    }
-
-
-#if EMULATE_SCHEDULER
-    private IEnumerator HandleCalculation(List<NodeModel> nodes) {
-        Debug.Log("Emulating Scheduler is enabled.");
-        yield return new WaitForSeconds(Random.Range(2, 6));
-
-        Debug.Log("Got result.");
-        ProcessCalculationResult(nodes);
-    }
-
-#else
-    private IEnumerator HandleCalculation(List<NodeModel> nodes) {
-        var request = SchedulerRestRequests.BuildPostShoppingListRequest(nodes);
-        yield return request.SendWebRequest();
-
-        if(request.responseCode != 200) {
-            Debug.LogWarning("Error: " + request.responseCode);
-            throw new System.NotImplementedException("No error handling for given response code while POST shopping list");
-
-        } else {
-            Debug.Log("Sending to scheduler successful.");
-            yield return CheckForCalculationResult();
-        }        
-    }
-
-    private IEnumerator CheckForCalculationResult() {
-        Debug.Log("Checking for result...");        
-        bool isCalculating = true;
-        List<NodeModel> result = null;
-
-        while(result == null && isCalculating) {
-            var request = SchedulerRestRequests.BuildGetCalculatedWaypointsRequest();
-            yield return new WaitForSeconds(2f);
-            yield return request.SendWebRequest();
-
-            if(!request.isNetworkError && request.responseCode == 200) {
-                Debug.Log("Got result.");
-
-                var response = Encoding.UTF8.GetString(request.downloadHandler.data);
-                result = JsonHelper.FromJson<NodeModel>(response);
-
-            } else if(request.isNetworkError /*|| request.responseCode == 503*/) {
-                Debug.LogWarning($"Cant get result. Network-Error: {request.isNetworkError}, Response-Code: {request.responseCode}");
-                isCalculating = false;
-            }
-        }
-
-        
-        ProcessCalculationResult(result);
-    }
-
-#endif //Emulate Scheduler
-
-    private void ProcessCalculationResult(List<NodeModel> result) {
-        if(result == null) {
-            Debug.Log("Got no result to display.");
-            OpenOpenerUi();
-            return;
-        }
-
-        CloseAllUis();
-        resultPanel.SetResult(result);
-        resultPanel.gameObject.SetActive(true);
     }
 
     public void OpenPlannerUi() {

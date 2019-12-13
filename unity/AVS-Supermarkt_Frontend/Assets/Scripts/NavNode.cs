d85d51db8a2ee9e9ce81bc68541ac5d81e45c17c
RedAvenge <thepowerredstone@gmail.com>
2019-12-13 20:36:01 +0100

    Add custom nav mesh points to the map

---
+++ unity/AVS-Supermarkt_Frontend/Assets/Scripts/NavNode.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(BoxCollider))]
+public class NavNode : MonoBehaviour {
+
+    [Header("NavNode-Settings")]
+    [Range(2, 10)] public int rangeRadius = 6;
+    public bool alwaysShowGizmos;
+
+
+    internal List<NavNode> nextNodes = new List<NavNode>();
+
+
+    protected virtual void Update() {
+        if(!EditorApplication.isPlaying && transform.hasChanged) {
+            nextNodes = new List<NavNode>();
+
+            Collider[] hits = Physics.OverlapSphere(ColliderCenter, rangeRadius, LayerMask.NameToLayer("Everything"), QueryTriggerInteraction.Collide);
+            foreach(var hit in hits) {
+                var node = hit.GetComponent<NavNode>();
+                if(node != null) {
+                    if(node == this) continue;
+                    if(this is ShopAsset && node is ShopAsset) continue;
+
+                    nextNodes.Add(node);
+                }
+            }
+        }
+    }
+
+    internal virtual Vector3 ColliderCenter => transform.position;
+
+    private void OnValidate() {
+        var bc = GetComponent<BoxCollider>();
+        bc.size = new Vector3(.1f, .1f, .1f);
+        bc.isTrigger = true;
+    }
+
+    private void OnDrawGizmos() {
+        if(alwaysShowGizmos) Gizmos.DrawSphere(ColliderCenter, .25f);
+
+        foreach(var node in nextNodes) {
+            Gizmos.color = Color.magenta;
+                      
+            if(!node.nextNodes.Contains(this)) {
+                Gizmos.color = Color.red;
+                Gizmos.DrawCube(ColliderCenter, Vector3.one); 
+            }
+
+            if(alwaysShowGizmos) Gizmos.DrawLine(ColliderCenter, node.ColliderCenter);
+        }
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.gray;
+        Gizmos.DrawWireSphere(ColliderCenter, rangeRadius);
+    }
+
+
+}
--- unity/AVS-Supermarkt_Frontend/Assets/Scripts/Shop/ShopAsset.cs
+++ unity/AVS-Supermarkt_Frontend/Assets/Scripts/Shop/ShopAsset.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public abstract class ShopAsset : MonoBehaviour {
+public abstract class ShopAsset : NavNode {
 
     /// <summary>
     /// Name of the shop asset / furniture.
@@ -20,10 +21,12 @@ public abstract class ShopAsset : MonoBehaviour {
 
 
     private TextMeshPro textPlane;
-    
-    protected virtual void Update() {
+
+    protected override void Update() {
+        base.Update();
+
 #if UNITY_EDITOR
-        if(transform.hasChanged && textPlane != null) {
+        if(!EditorApplication.isPlaying && transform.hasChanged && textPlane != null) {
             var rect = textPlane.GetComponent<RectTransform>();
             if(transform.localScale.x != 0 && transform.localScale.z != 0) {
                 rect.localScale = new Vector3(1 / transform.localScale.x, 1 / transform.localScale.z, 1);
@@ -32,11 +35,19 @@ public abstract class ShopAsset : MonoBehaviour {
 #endif
     }
 
+    internal override Vector3 ColliderCenter => WalkToPoint;
+
+
 
 #if UNITY_EDITOR
-    protected virtual void OnDrawGizmosSelected() {
+    protected virtual void OnDrawGizmos() {
         Gizmos.color = Color.grey;
-        Gizmos.DrawSphere(WalkToPoint, .5f);
+        Gizmos.DrawSphere(WalkToPoint, .4f);
+
+        if(nextNodes.Count <= 0) {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(WalkToPoint, .8f);
+        }
     }
 
     protected virtual void OnValidate() {
@@ -44,6 +55,18 @@ public abstract class ShopAsset : MonoBehaviour {
         if(textPlane != null) textPlane.text = AssetName;
 
         name = $"{GetType().Name} ({AssetName})";
+
+        var bc = GetComponent<BoxCollider>();
+        if(bc) {          
+            bc.isTrigger = true;           
+            bc.center = transform.InverseTransformPoint(WalkToPoint);
+
+            var scale = transform.lossyScale;
+            if(scale.x == 0) scale.x = 1;
+            if(scale.y == 0) scale.y = 1;
+            if(scale.z == 0) scale.z = 1;
+            bc.size = new Vector3(.6f / scale.x, .6f / scale.y, .6f / scale.z);
+        }
     }
 #endif
 
